13b94faca37364e17a2821ac8bb7195e4e9fb3df
hoangdtcg <hoang.du@codegym.vn>
2022-06-08 08:54:13 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        DB::table('users')->truncate();
+
+        DB::table('users')->insert([
+            'name'     => 'Demo Admin',
+            'email'    => 'admin@example.com',
+            'password' => bcrypt('123456'),
+            'is_admin' => 1,
+        ]);
+    }
+}
